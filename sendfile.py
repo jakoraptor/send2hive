07abafb8d9ecaa555f192b84bbbe116179b46f10
jakoraptor <77681812+jakoraptor@users.noreply.github.com>
2022-07-28 16:44:03 +0100

    Update sendfile.py

--- sendfile.py
+++ sendfile.py
@@ -1,18 +1,18 @@
+#!/usr/bin/python3
+
 import sys
 import json
-import os
+import keyring
 from thehive4py.api import TheHiveApi
 from thehive4py.models import CaseObservable
 
-hiveurl='***HIVEURL AND PORT***' # <<< CHANGE THIS
-apikey=os.environ.get('hiveAPIkey')
-
-api = TheHiveApi(hiveurl, apikey)
-
-
-if len(sys.argv) < 3:
-    print('Usage: python3 sendfile.py "filename" "case id"\nBefore running, add valid api key to environment variable "hiveAPIkey" using "export hiveAPIkey=***KEY***"')
+if len(sys.argv) != 4:
+    print('Usage: python3 sendfile.py "filename" "user" "case id"\nBefore running, add valid user api key to keyring "hiveapi" eg. "keyring set hiveapi <username>"')
     sys.exit(1)
+
+hiveurl="https://localhost:8000" # <<< CHANGE THIS
+apikey=keyring.get_password("hiveapi",sys.argv[2])
+api = TheHiveApi(hiveurl, apikey) # <<< ADD cert=False HERE IF SSL NOT USED
 
 try:
     # Init the CaseObservable object
@@ -22,11 +22,11 @@ try:
         ioc=False,
         sighted=True,
         tags=['thehive4py'],
-        message='test'
+        message='Observable sent using thehive4py'
     )
 
     # Call the API
-    response = api.create_case_observable(sys.argv[2], file_observable)
+    response = api.create_case_observable(sys.argv[3], file_observable)
 
     # Display the results
     if response.status_code == 201:
@@ -36,12 +36,12 @@ try:
         # Display response data
         print(json.dumps(observableJson, indent=4, sort_keys=True))
     elif response.status_code == 401:
-        print('Usage: python3 sendfile.py "filename" "case id"')
-        print('Authentication Failure: Check hiveAPIkey environment variable has valid key')
+        print('Usage: python3 sendfile.py "filename" "user" "case id"')
+        print('Authentication Failure: Check user API key in keyring - Example: "keyring set hiveapi <username>"')
     else:
-        print('Usage: python3 sendfile.py "filename" "case id"')
+        print('Usage: python3 sendfile.py "filename" "user" "case id"')
         print(f'Failure: {response.status_code}/{response.text}')
 
     sys.exit(0)
 except OSError as e:
-    print(f'Usage: python3 sendfile.py "filename" "case id"\n{e}')
+    print(f'Usage: python3 sendfile.py "filename" "user" "case id"\n{e}')
